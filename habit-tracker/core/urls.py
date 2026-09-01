1eae0ff83c5d7fa80f1aa87ea8f3109c63998b7a
Jevis Kafle <jeviskafle@gmail.com>
2026-09-01 13:45:33 +0545

    Health check for server

--- habit-tracker/core/urls.py
+++ habit-tracker/core/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import health_check
 
 urlpatterns = [
+    path("api/health/", health_check, name="health-check"),
     path("admin/", admin.site.urls),
     path("api/", include("habits.urls")),
     path("api/auth/", include("accounts.urls")),
